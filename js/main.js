849d011878c1576e05a96fe0a08e886486fa5d70
tanmayagrawal <tanmay.omshanti.agrawal@gmail.com>
2017-10-09 23:37:29 +0530

    Updating Projects tab

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function run() {
+	var acc = document.getElementsByClassName("custom-accordion");
+
+	for (var i = 0; i < acc.length; i++) {
+	    acc[i].onclick = function() {
+	        this.classList.toggle("active");
+	        var panel = this.nextElementSibling;
+	        if (panel.style.display === "block") {
+	            panel.style.display = "none";
+	        } else {
+	            panel.style.display = "block";
+	        }
+	    }
+	}
+}
+
+window.onload = function() {
+	run();
+}
